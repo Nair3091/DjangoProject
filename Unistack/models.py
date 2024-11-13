b705418c14edb446bb038d67c0c3eadad6ef1092
Dhruv Nair <dhruv14nair@gmail.com>
2024-11-13 14:35:38 +0530

    i promise this is the final commit (maybe not)

--- Unistack/models.py
+++ Unistack/models.py
@@ -1,18 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
     content = models.TextField()
+    file = models.FileField(upload_to='uploads/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    code_snippet = models.TextField(blank=True, null=True)  # Add this field
-    code_file = models.FileField(upload_to='uploads/', blank=True, null=True)  # Add this field for file upload
+
+    def __str__(self):
+        return self.title
 
 
 class Answer(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='answers')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    file = models.FileField(upload_to='uploads/', blank=True, null=True)  # New field for file upload
+    file = models.FileField(upload_to='uploads/', blank=True, null=True)
 
     def __str__(self):
         return self.content[:50]
